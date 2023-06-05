cf94f677b16f7f63941495109e2f5c543d7d0a82
EAGDEV200 <127441932+EAGDEV200@users.noreply.github.com>
2023-06-05 19:44:11 -0300

    Commit final por diogo (#1)
    * Add files via upload
    
    Trabalho realizado por Diogo Stocco Woehl 25777297 e Eduardo Almeida de Gouveia 26254689
    
    * Update README.md
    
    ---------
    
    Co-authored-by: DiogoStocco <102595422+DiogoStocco@users.noreply.github.com>

---
+++ src/components/Card/index.js
@@ -0,0 +1,81 @@
+import { useEffect, useState } from 'react';
+import { Link,  } from 'react-router-dom';
+
+
+function Card({ filme }) {
+  const [assistido, setAssistido] = useState(false);
+
+  const handleAssistido = () => {
+    setAssistido(!assistido);
+  }
+
+  return (
+    <div className="col-md-4 mb-3">
+      <div className="card">
+        <img src={filme.poster} alt={filme.titulo} className="card-img-top" />
+        <div className="card-body">
+          <h5 className="card-title">{filme.titulo} ({filme.ano})</h5>
+          <p className="card-text">{filme.sinopse}</p>
+          <p className="card-text">Nota: {filme.nota}</p>
+          <Link to={`/filme/${filme.id}`}>
+          <button onClick={handleAssistido} className="btn btn-sm btn-primary">
+            {assistido ? 'Assistir' : 'Assistir Novamente'}
+          </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function App() {
+  const [filmes, setFilmes] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [ordenacao, setOrdenacao] = useState('default');
+
+  useEffect(() => {
+    fetch('https://my-json-server.typicode.com/marycamila184/movies/movies')
+      .then(response => response.json())
+      .then(data => setFilmes(data))
+      .catch(error => console.error(error));
+  }, []);
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredFilmes = filmes
+    .filter((filme) => filme.titulo.toLowerCase().includes(searchTerm.toLowerCase()))
+    .sort((a, b) => {
+      if (ordenacao === 'titulo') {
+        return a.titulo.localeCompare(b.titulo);
+      } else if (ordenacao === 'nota') {
+        return b.nota - a.nota;
+      } else if (ordenacao === 'ano-asc') {
+        return a.ano - b.ano;
+      } else if (ordenacao === 'ano-desc') {
+        return b.ano - a.ano;
+      }
+      return 0;
+    });
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12 mb-3">
+          <input type="text" placeholder="Pesquisar filmes" onChange={handleSearchChange} value={searchTerm} />
+          <select value={ordenacao} onChange={(event) => setOrdenacao(event.target.value)}>
+            <option value="default">Ordem original</option>
+            <option value="titulo">Ordem alfabética</option>
+            <option value="nota">Ordem por nota (maior para menor)</option>
+            <option value="ano-asc">Ordem por ano de lançamento (mais antigos primeiro)</option>
+            <option value="ano-desc">Ordem por ano de lançamento (mais recentes primeiro)</option>
+          </select>
+        </div>
+        {filteredFilmes.map(filme => <Card key={filme.id} filme={filme} />)}
+      </div>
+    </div>
+  );
+}
+
+export default App;
